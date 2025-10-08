9a07c416bf853a18083caab458c07b09b19e51a2
Dhairya Savlekar <dhairyasavlekar16@gmail.com>
2025-10-08 16:05:47 +0530

    header changes

--- app/page.js
+++ app/page.js
@@ -2,5 +2,9 @@ import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
-  return <Button>hi</Button>
+  return (
+    <div className="mt-50">
+      <Button>hi</Button>
+    </div>
+  )
 }
