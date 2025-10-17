3fd4eca20c27f2359d7d50aab48dfc7f50c0b156
Dhairya Savlekar <dhairyasavlekar16@gmail.com>
2025-10-17 16:56:00 +0530

    create transaction

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -1,5 +1,23 @@
-const AddTransaction = () => {
-  return <div>add transaction</div>
+import { getUserAccounts } from '@/actions/dashboard'
+import TransactionForm from '@/app/(main)/transaction/_components/transaction-form'
+import { defaultCategories } from '@/data/categories'
+
+const AddTransactionPage = async () => {
+  const accounts = await getUserAccounts()
+
+  return (
+    <div className="max-w-3xl mx-auto px-5">
+      <div className="flex justify-center md:justify-normal mb-8">
+        <h1 className="text-5xl gradient-title ">Add Transaction</h1>
+      </div>
+      <TransactionForm
+        accounts={accounts}
+        categories={defaultCategories}
+        // editMode={!!editId}
+        // initialData={initialData}
+      />
+    </div>
+  )
 }
 
-export default AddTransaction
+export default AddTransactionPage
