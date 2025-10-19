c9b147b07ebfe06c08996bcc40edff695ed90d43
Dhairya Savlekar <dhairyasavlekar16@gmail.com>
2025-10-19 23:22:52 +0530

    handle recurring transactions

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,10 @@
-import { serve } from 'inngest/next'
 import { inngest } from '@/lib/inngest/client'
-import { checkBudgetAlerts } from '@/lib/inngest/functions'
+import {
+  checkBudgetAlerts,
+  processRecurringTransaction,
+  triggerRecurringTransactions,
+} from '@/lib/inngest/functions'
+import { serve } from 'inngest/next'
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
@@ -8,5 +12,7 @@ export const { GET, POST, PUT } = serve({
   functions: [
     /* your functions will be passed here later! */
     checkBudgetAlerts,
+    triggerRecurringTransactions,
+    processRecurringTransaction,
   ],
 })
